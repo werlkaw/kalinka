71350c072e7929b7c9eed0dc931c2d63669410a3
Juan Bustamante <jab675@cornell.edu>
2020-02-27 14:30:54 -0800

    implement twilio server and skeleton api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,28 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp(functions.config().firebase);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+
+/* Express */
+const main = express();
+
+const api = express();
+api.get('*', (req, resp) => {
+    resp.send('api is working!');
 });
+
+main.use('/api/v1', api);
+main.post('/twilio', (req, resp) => {
+    const phone: String = req.body['From'];
+    const message: String = req.body['Body'];
+    const twilioResponse = new MessagingResponse();
+    twilioResponse.message(`I saw: ${message} from ${phone}`);
+
+    resp.writeHead(200, {'Content-Type': 'text/xml'});
+    resp.end(twilioResponse.toString());
+});
+
+export const kalinka = functions.https.onRequest(main);
